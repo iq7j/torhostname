f3e4c4e807b3bcd8b715441e1bbed92510a209d5
Quack <79198231+iq7j@users.noreply.github.com>
2021-05-12 16:03:20 +0300

    Add files via upload

---
+++ tor.sh
@@ -0,0 +1,44 @@
+apt update && apt upgrade
+apt install tor
+apt install nginx
+
+echo """                                                                                                    
+                                            #%%%%%%%%%%%%%%%%%%/                                    
+                                       #%%%%%%%%%#*   .,,(%%%%%%%%%%(                               
+                                   .%%%%%%%.                    .%%%%%%%                            
+                                 %%%%%%                             *%%%%%/                         
+                               %%%%%/                                  %%%%%.                       
+                             .%%%%                                       %%%%%                      
+                            #%%%%                        , .               %%%%                     
+                           *%%%%  %%%%%%%%%%%%%%%%     /(/                  %%%%                    
+                           %%%%   %%%%%%%%%%%%%%%%   %((                    .%%%%                   
+                          (%%%*        %%%%%%        @ #@          #%%%%%/   %%%%                   
+                          %%%%         %%%%%%     @    ,*//&.     %%%%%%%/   %%%%                   
+                          %%%%         %%%%%%   # .   ..#@(((#   %%%%%%.     %%%%                   
+                          ,%%%#        %%%%%%  @ /     ,,*####@  %%%%%%      %%%%                   
+                           %%%%        %%%%%%  , *   (.%%&###%#  %%%%%%     #%%%*                   
+                            %%%%       %%%%%%    @   ..%&%%%@    %%%%%%    #%%%%                    
+                             %%%%                                         %%%%%                     
+                              %%%%                                      *%%%%.                      
+                            %%%%%                                     %%%%%#                        
+                         #%%%%%. .*%%%%%.                          #%%%%%*                          
+                          %%%%%%%%%%%%%%%%%%%                 .%%%%%%%#                             
+                                        .%%%%%%%%%%%%%%%%%%%%%%%%%%                                 
+                                               (%%%%%%%%%%%%,                                       """
+
+
+
+
+echo "welcome,this tool will help you to create a  .onion website"
+read -p "now this tool will get you to a file uncomment  Hiddenservicedir,hiddenserviceport:"
+tor service start
+nano etc/tor/torrc
+read -p "next...you will get your hostname soon:"
+tor service stop
+tor service start
+nano /var/lib/tor/hidden_service/hostname
+echo "congrats this is your hostname now(:"
+read -p "and now we will start nginx"
+nginx service start
+nginc service status
+echo "now it should be working..."
