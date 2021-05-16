d3d4de46a1df935ad6026566d60f7bcabf4562ca
Quack <79198231+iq7j@users.noreply.github.com>
2021-05-16 14:16:21 +0300

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade
 apt install tor
 apt install nginx
-
+touch yourhostname.txt
 echo """                                                                                                    
                                             #%%%%%%%%%%%%%%%%%%/                                    
                                        #%%%%%%%%%#*   .,,(%%%%%%%%%%(                               
@@ -33,11 +33,10 @@ echo "welcome,this tool will help you to create a  .onion website"
 read -p "now this tool will get you to a file uncomment  Hiddenservicedir,hiddenserviceport:"
 tor service start
 nano etc/tor/torrc
-read -p "next...you will get your hostname soon:"
+read -p "next...this tool will save your hostname in yourhostname.txt:"
 tor service stop
 tor service start
-nano /var/lib/tor/hidden_service/hostname
-echo "congrats this is your hostname now(:"
+cat /var/lib/tor/hidden_service/hostname > yourhostname.txt
 read -p "and now we will start nginx"
 service nginx start
 service nginx status
